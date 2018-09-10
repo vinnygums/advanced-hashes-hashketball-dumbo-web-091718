2eb4be71ae3e15124a360de76a3da1a926fc9ec6
Vicente Soriano <vicente.soriano@icloud.com>
2018-09-10 14:43:41 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -141,6 +141,7 @@ def shoe_size(player)
       value = attribute[:players]
       binding.pry
       #if info == :players
+      
         value.each do |player_name, game_stat|
           if player_name == player
             game_stat.each do |stats, number|
